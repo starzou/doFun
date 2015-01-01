b8bbc9934879a92f87aaaf76fbb72458c1a27a51
starzou <starzou@126.com>
2015-01-01 17:20:29 +0800

    重构

--- www/js/app.js
+++ www/js/app.js
@@ -1,102 +1,108 @@
-// Ionic funApp App
+/**
+ * @class app
+ * @description funApp 应用
+ * @time 2015-01-01 17:13
+ * @author StarZou
+ **/
+(function (window, document) {
+    'use strict';
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'funApp' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'funApp.services' is found in services.js
-// 'funApp.controllers' is found in controllers.js
-angular.module('funApp', ['ionic', 'funApp.controllers', 'funApp.services'])
+    /**
+     * 定义 funApp 应用
+     */
+    var funApp = angular.module('funApp', ['ionic', 'funApp.controllers', 'funApp.services']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
 
-.config(function($stateProvider, $urlRouterProvider) {
+    /**
+     * 配置平台
+     */
+    funApp.run(['$ionicPlatform', function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    }]);
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+    /**
+     * 配置路由
+     */
+    funApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-  // Each tab has its own nav history stack:
+        $stateProvider
+            .state('tab', {
+                url        : "/tab",
+                abstract   : true,
+                templateUrl: "templates/tabs.html"
+            })
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+            .state('tab.dash', {
+                url  : '/dash',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/tab-dash.html',
+                        controller : 'DashCtrl'
+                    }
+                }
+            })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+            .state('tab.chats', {
+                url  : '/chats',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'templates/tab-chats.html',
+                        controller : 'ChatsCtrl'
+                    }
+                }
+            })
+            .state('tab.chat-detail', {
+                url  : '/chats/:chatId',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'templates/chat-detail.html',
+                        controller : 'ChatDetailCtrl'
+                    }
+                }
+            })
 
-  .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
+            .state('tab.friends', {
+                url  : '/friends',
+                views: {
+                    'tab-friends': {
+                        templateUrl: 'templates/tab-friends.html',
+                        controller : 'FriendsCtrl'
+                    }
+                }
+            })
+            .state('tab.friend-detail', {
+                url  : '/friend/:friendId',
+                views: {
+                    'tab-friends': {
+                        templateUrl: 'templates/friend-detail.html',
+                        controller : 'FriendDetailCtrl'
+                    }
+                }
+            })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+            .state('tab.account', {
+                url  : '/account',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/tab-account.html',
+                        controller : 'AccountCtrl'
+                    }
+                }
+            });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+        $urlRouterProvider.otherwise('/tab/dash');
+    }]);
 
-});
+
+})(window, document);
+
+
+
