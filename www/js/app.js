2bbffee8d5dd13362d1d4005d868ddab2923a3fe
starzou <starzou@126.com>
2015-01-01 17:11:40 +0800

    重构

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,11 @@
-// Ionic Starter App
+// Ionic funApp App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'funApp' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'funApp.services' is found in services.js
+// 'funApp.controllers' is found in controllers.js
+angular.module('funApp', ['ionic', 'funApp.controllers', 'funApp.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
